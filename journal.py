384cdf763bfb16e2146c8b00315d21cbff4cad09
Andrew Pungprakearti <andrew@alephsf.com>
2021-01-26 16:37:54 -0800

    Finished entry creation and reading

--- journal.py
+++ journal.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
 '''
-journal.py is script that encodes a journal/text file using a key
+journal.py is a script that encrypt and decrypts journal entries
 '''
 
 import getpass
 import bcrypt
 from Crypto.Cipher import AES
 import os
-import os.path
-from os import path
 from datetime import date
 from datetime import datetime
 import sys
@@ -22,22 +20,44 @@ PASSWORD = ''
 HASHED_PASSWORD = ''
 
 
+def out(mode, message):
+    ''' Print messages in special colors '''
+
+    warn = '\033[31m' # Red
+    success = '\033[96m' # Cyan
+    question = '\033[95m' # Purple
+    reset = '\033[00m'
+
+    if(mode == 'warn'):
+        print(f'{warn}{message}{reset}')
+
+    if(mode == 'success'):
+        print(f'{success}{message}{reset}')
+
+    # This is for inputs
+    if(mode == 'question'):
+        return input(f'{question}{message}{reset}')
+
+
 def create_password():
     ''' Create main password to protect journal entries '''
 
     global KEY
     global PASSWORD
 
-    # Create Journal directory
-    if(not path.exists('./journal')):
+    # Create Journal directories
+    if(not os.path.exists('./journal')):
         os.mkdir('./journal')
+
+    if(not os.path.exists('./journal/entries')):
+        os.mkdir('./journal/entries')
 
     # Create a new password
     password = getpass.getpass('Please enter a new password: ')
     confirm_password = getpass.getpass('Please confirm your password: ')
 
     if(password == confirm_password):
-        print('PASSWORD CONFIRMED')
+        out('success', 'PASSWORD CONFIRMED')
 
         # Save password locally in secret and assign to global
         PASSWORD = password
@@ -47,7 +67,7 @@ def create_password():
         KEY = pad_string(PASSWORD, 'key')
 
     else:
-        print('ERROR: THOSE PASSWORDS DO NOT MATCH\n')
+        out('warn', 'ERROR: THOSE PASSWORDS DO NOT MATCH\n')
         create_password()
 
 
@@ -59,6 +79,7 @@ def get_password():
 
     # Get password and assign to global
     password = getpass.getpass('Please enter your password: ')
+    print('')
     PASSWORD = password
 
     if(password_verify()):
@@ -66,7 +87,7 @@ def get_password():
         KEY = pad_string(PASSWORD, 'key')
 
     else:
-        print('UNAUTHORIZED: PASSWORD INCORRECT')
+        out('warn', 'UNAUTHORIZED: PASSWORD INCORRECT')
         exit()
 
 
@@ -83,7 +104,7 @@ def save_password_local(password):
     secret_file.write(hashed_password)
     secret_file.close()
 
-    print('NEW PASSWORD SAVED')
+    out('success', 'NEW PASSWORD SAVED')
 
 
 def password_verify():
@@ -114,10 +135,10 @@ def pad_string(string, mode='entry'):
 
     # Snippet for index
     elif(mode == 'snippet'):
-        if(len(string) > 32):
-            formatted = string[:32]
+        if(len(string) > 96):
+            formatted = string[:96]
         else:
-            formatted = (string + '                                ')[:32]
+            formatted = (string + '                                                                                                ')[:96]
 
     # Journal entry
     else:
@@ -129,8 +150,6 @@ def pad_string(string, mode='entry'):
 
 def encrypt(message):
     ''' Encrypt a message using AES '''
-
-    print(message, len(message))
 
     cipher_encrypt = AES.new(KEY, AES.MODE_CBC, IV)
     cipher_text = cipher_encrypt.encrypt(message)
@@ -146,25 +165,24 @@ def decrypt(message):
 
     return decrypted_message.decode('utf-8')
 
-# NEED TO FIGURE OUT BETTER PADDING
-# LENGTH IS IN CORRECT WHEN TRYING TO ENCRYPT
+
 def create_entry():
     ''' Check for entry date and if file exists, add entry to it '''
 
     get_password()
 
     today = date.today()
-    filename = today.strftime('%Y%m%d')
     readable_date = today.strftime('%B %d, %Y')
     now = datetime.now()
     current_time = now.strftime('%H:%M:%S')
+    filename = today.strftime('%Y%m%d') + now.strftime('%H%M%S')
 
     # Get new entry
-    new_entry = input(f'{readable_date}: {current_time} - ')
+    new_entry = out('question', f'{readable_date}: {current_time} - ')
     new_entry = f'{readable_date}: {current_time} - ' + new_entry
 
     # Read index and place new entry before old data
-    if(path.exists('./journal/index')):
+    if(os.path.exists('./journal/index')):
         index = open('./journal/index', 'rb')
         old_entries = decrypt(index.read())
         index.close()
@@ -172,24 +190,75 @@ def create_entry():
         old_entries = ''
 
     new_entry_padded = pad_string(new_entry, 'snippet')
-    index_text = pad_string(new_entry + '\n' + old_entries)
+    index_text = pad_string(new_entry_padded + '\n' + old_entries)
 
     index = open('./journal/index', 'wb')
     index.write(encrypt(index_text))
     index.close()
 
     # Create entry file
-    '''
-    entry_file = open(f'./journal/entries/{filename}', 'rb')
-    old_entry_contents = entry_file.read()
-    entry_file.close()
-
     entry_file = open(f'./journal/entries/{filename}', 'wb')
-    old_entry_contents = decrypt(old_entry_contents)
-    entry_to_write = pad_string(new_entry) + old_entry_contents
+    entry_to_write = pad_string(new_entry)
     entry_file.write(encrypt(entry_to_write))
     entry_file.close()
+
+    out('success', '\nENTRY CREATED AND ENCRYPTED')
+
+
+def list_entries():
     '''
+    List all entries with snippets and have user select which
+    entry to show.
+    '''
+
+    get_password()
+
+    # Read index file and list entries
+    index = open('./journal/index', 'rb')
+    index_contents = decrypt(index.read())
+    index.close()
+    index_sections = index_contents.split('\n')
+
+    i = 1
+    for section in index_sections[:-1]:
+        print(f'\033[95m{i}.\033[00m {section}')
+        i += 1
+
+    choice = out('question', '\nPlease select an entry to read: ')
+
+    # convert choice to integer
+    try:
+        choice = int(choice)
+    except ValueError:
+        out('warn', 'ERROR: Your choice should be a listed number')
+        exit()
+
+    if(choice > 0 and choice < len(index_sections)):
+        read_entry(choice)
+    else:
+        out('warn', 'ERROR: Your choice should be a listed number')
+
+
+def read_entry(entry_number):
+    ''' Use entry number to decrupt that entry in the entries folder '''
+
+    # Get all entry files and sort them in reverse order
+    entries = os.listdir('./journal/entries')
+
+    if(len(entries) < 1):
+        out('warn', 'ERROR: There are no entries')
+        exit()
+
+    entries.sort(reverse=True)
+
+    # Open, decrypt and print contents of entry
+    entry_file = open(f'./journal/entries/{entries[entry_number - 1]}', 'rb')
+    entry = entry_file.read()
+    entry_file.close()
+    entry_decrypted = decrypt(entry)
+    entry_list = entry_decrypted.split(' - ')
+
+    print('\n\033[96m' + entry_list[0] + ' - \033[00m' + entry_list[1] + '\n')
 
 
 def print_help():
@@ -197,39 +266,15 @@ def print_help():
 
     print('''\n./journal.py <option>
 ------------------------------------------------------
-help, h, ?              Display this menu
-create                  Create a new journal entry
-list                    List all entries with snippets
-read <list number>      Display entry
+\033[95mhelp, h, ?\033[00m              Display this menu
+\033[95mcreate\033[00m                  Create a new journal entry
+\033[95mlist\033[00m                    List all entries with snippets
+\033[95mread <list number>\033[00m      Display entry
 ''')
 
 
-def list_entries():
-    get_password()
-
-    index = open('./journal/index', 'rb')
-    index_contents = decrypt(index.read())
-    print('this is index: ', index_contents)
-    index.close()
-
-
-def read_entry(entry_number):
-    print('read entry')
-
-
-#################################################
-
-# Verify current password of if none exists, create a new one
-# get_password()
-
-# Create initial journal file and directory
-# if(not path.exists('./journal/journal')):
-    # create_journal()
-
-# Create entry
-# create_entry()
-
-
+##################################################################################################
+''' Run script '''
 
 options = {
     'help': print_help,
@@ -240,24 +285,17 @@ options = {
     'read': read_entry
 }
 
-if(path.exists('./journal/secret')):
+if(os.path.exists('./journal/secret')):
     if(len(sys.argv) == 1):
         print_help()
 
     if(len(sys.argv) == 2):
-        options[sys.argv[1]]()
+        try:
+            options[sys.argv[1]]()
+        except KeyError:
+            out('warn', 'ERROR: That operation does not exist')
+            print_help()
 
 else:
-    print('INITIALIZING JOURNAL')
+    out('success', 'INITIALIZING JOURNAL')
     create_password()
-
-'''
-journal file
-first line is hash
-every line after that is path to files
-files organized by date 20210122
-
-save hashed password to verify because if we just encrypt with
-any password, if the password is different, if will be very difficult
-to retrieve your data
-'''
